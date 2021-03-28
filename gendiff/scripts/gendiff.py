7b6b06d893c02e712fa33c9a83dee9822783cbef
denis <yudaev1@gmail.com>
2021-03-28 16:33:28 +0300

    Create scripts/gendiff.py

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+args = parser.parse_args()
+
+
+
