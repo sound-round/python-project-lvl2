3c202270884ee6bbed19a3177076251c469393a9
denis <yudaev1@gmail.com>
2021-04-04 14:13:42 +0300

    Sort lines

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,7 +5,7 @@ def generate_diff(first_file, second_file):
     lines = []
     INDENT = '  '
 
-    for key, value in first_file.items():
+    for key, value in sorted(first_file.items()):
         if first_file.get(key) == second_file.get(key):
             lines.append(f'{INDENT}  {key}: {value}')
         else:
@@ -15,7 +15,7 @@ def generate_diff(first_file, second_file):
             else:
                 lines.append(f'{INDENT}- {key}: {value}')
 
-    for key, value in second_file.items():
+    for key, value in sorted(second_file.items()):
         if key in first_file:
             continue
         else:
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,12 +6,12 @@ from gendiff import gendiff
 import json
 
 
-
 parser = argparse.ArgumentParser(description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument('-f', '--format', help='set format of output')
 args = parser.parse_args()
+
 
 def main():
     first_file = json.load(open(args.first_file))
