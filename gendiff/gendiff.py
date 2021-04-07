35d285efb8dc84464fc5eca4d31d9d71e5833674
sound-round <yudaev1@gmail.com>
2021-04-07 10:26:45 +0300

    Fix necessary result

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 
 
 def generate_diff(first_file, second_file):
@@ -7,20 +8,20 @@ def generate_diff(first_file, second_file):
 
     for key, value in sorted(first_file.items()):
         if first_file.get(key) == second_file.get(key):
-            lines.append(f'{INDENT}  {key}: {value}')
+            lines.append(f'{INDENT}  {key}: {json.dumps(value)}')
         else:
             if key in second_file:
-                lines.append(f'{INDENT}- {key}: {value}')
+                lines.append(f'{INDENT}- {key}: {json.dumps(value)}')
                 lines.append(f'{INDENT}+ {key}: {second_file.get(key)}')
             else:
-                lines.append(f'{INDENT}- {key}: {value}')
+                lines.append(f'{INDENT}- {key}: {json.dumps(value)}')
 
     for key, value in sorted(second_file.items()):
         if key in first_file:
             continue
         else:
-            lines.append(f'{INDENT}+ {key}: {value}')
+            lines.append(f'{INDENT}+ {key}: {json.dumps(value)}')
 
-    result = '\n'.join(itertools.chain('{', lines, '}'))
+    result = '\n'.join(itertools.chain('{', lines, '}')).replace('"', '')
     print(result)
     return result
