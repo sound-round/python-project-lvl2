bffa8ea29462f40665dfae74e9c9e18d31bf3aa6
sound-round <yudaev1@gmail.com>
2021-04-08 11:20:12 +0300

    Update tests/

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,6 +1,7 @@
 from gendiff.gendiff import generate_diff
 import pytest
 import json
+import yaml
 
 
 def read(file_path):
@@ -9,8 +10,15 @@ def read(file_path):
     return result
 
 
-def test_generate_diff():
+def test_generate_diff_json():
     assert generate_diff(
         json.load(open('tests/fixtures/file1.json')),
         json.load(open('tests/fixtures/file2.json')),
+    ) == str(read('tests/fixtures/result.txt'))
+
+
+def test_generate_diff_yaml():
+    assert generate_diff(
+        yaml.load(open('tests/fixtures/file1.yaml')),
+        yaml.load(open('tests/fixtures/file2.yaml')),
     ) == str(read('tests/fixtures/result.txt'))
