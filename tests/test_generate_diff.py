859e3d4c0e937514955997eb87f2b748717d1ca7
sound-round <yudaev1@gmail.com>
2021-04-09 11:32:52 +0300

    Update tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,7 +1,5 @@
 from gendiff.gendiff import generate_diff
 import pytest
-import json
-import yaml
 
 
 def read(file_path):
@@ -10,14 +8,30 @@ def read(file_path):
     return result
 
 
-def test_generate_diff_json():
-    assert generate_diff(
+@pytest.mark.parametrize('first_file, second_file, expected_result', [
+    (
         'tests/fixtures/file1_plain.json',
         'tests/fixtures/file2_plain.json',
-    ) == str(read('tests/fixtures/result_plain.txt'))
-
-def test_generate_diff_yaml():
-    assert generate_diff(
+        'tests/fixtures/result_plain.txt',
+    ),
+    (
         'tests/fixtures/file1_plain.yaml',
         'tests/fixtures/file2_plain.yaml',
-    ) == str(read('tests/fixtures/result_plain.txt'))
+        'tests/fixtures/result_plain.txt',
+    ),
+    (
+        'tests/fixtures/file1_nested.json',
+        'tests/fixtures/file2_nested.json',
+        'tests/fixtures/result_nested.txt',
+    ),
+    (
+        'tests/fixtures/file1_nested.yaml',
+        'tests/fixtures/file2_nested.yaml',
+        'tests/fixtures/result_nested.txt',
+    )
+])
+def test_generate_diff(first_file, second_file, expected_result):
+    assert generate_diff(
+        first_file,
+        second_file,
+    ) == str(read(expected_result))
