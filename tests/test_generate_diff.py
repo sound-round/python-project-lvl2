fa2e75c52cdd5f0ca2263566ef894b4a70bb9e77
denis <yudaev1@gmail.com>
2021-05-11 15:06:24 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -10,16 +10,6 @@ def read(file_path):
 
 @pytest.mark.parametrize('first_file, second_file, expected_result', [
     (
-        'tests/fixtures/simple/file1_simple.json',
-        'tests/fixtures/simple/file2_simple.json',
-        'tests/fixtures/result_simple.txt',
-    ),
-    (
-        'tests/fixtures/simple/file1_simple.yaml',
-        'tests/fixtures/simple/file2_simple.yaml',
-        'tests/fixtures/result_simple.txt',
-    ),
-    (
         'tests/fixtures/nested/file1_nested.json',
         'tests/fixtures/nested/file2_nested.json',
         'tests/fixtures/result_nested_stylish.txt',
@@ -35,7 +25,21 @@ def test_generate_diff_stylish(first_file, second_file, expected_result):
         first_file,
         second_file,
         format_name='stylish',
-    ) == str(read(expected_result))
+    ) == read(expected_result)
+
+
+@pytest.mark.parametrize('first_file, second_file, expected_result', [
+    (
+        'tests/fixtures/nested/file1_nested.json',
+        'tests/fixtures/nested/file2_nested.json',
+        'tests/fixtures/result_nested_stylish.txt',
+    ),
+])
+def test_generate_diff_stylish_default(first_file, second_file, expected_result):
+    assert generate_diff(
+        first_file,
+        second_file,
+    ) == read(expected_result)
 
 
 @pytest.mark.parametrize('first_file, second_file, expected_result', [
@@ -55,7 +59,7 @@ def test_generate_diff_plain(first_file, second_file, expected_result):
         first_file,
         second_file,
         format_name='plain',
-    ) == str(read(expected_result))
+    ) == read(expected_result)
 
 
 @pytest.mark.parametrize('first_file, second_file, expected_result', [
@@ -75,4 +79,4 @@ def test_generate_diff_json(first_file, second_file, expected_result):
         first_file,
         second_file,
         format_name='json',
-    ) == str(read(expected_result))
+    ) == read(expected_result)
