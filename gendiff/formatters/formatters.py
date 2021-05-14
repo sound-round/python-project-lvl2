1e79ea2e3dc09c8fe7d1b989d8443303ab191cc8
denis <yudaev1@gmail.com>
2021-05-14 18:27:20 +0300

    Fix linter error scripts.gendiff.py

--- gendiff/formatters/formatters.py
+++ gendiff/formatters/formatters.py
@@ -1,12 +1,6 @@
 from gendiff.formatters import stylish_format, plain_format, json_format
 
 
-map_bool = {
-    True: 'true',
-    False: 'false',
-    None: 'null',
-}
-
 FORMATTERS = {
     'stylish': stylish_format,
     'plain': plain_format,
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,5 +11,6 @@ def main():
     diff = gendiff.generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
+
 if __name__ == '__main__':
     main()
