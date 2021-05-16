30a4f9eb81b88167536c9ad2685c20df71061695
denis <yudaev1@gmail.com>
2021-05-16 16:26:27 +0300

    Update representation.py

--- gendiff/representation.py
+++ gendiff/representation.py
@@ -4,45 +4,46 @@
 def generate(node1, node2):
 
     diff = []
+    all_keys = node1.keys() | node2.keys()
     common_keys = node1.keys() & node2.keys()
     removed_keys = node1.keys() - node2.keys()
-    added_keys = node2.keys() - node1.keys()
 
-    for key in common_keys:
-        if isinstance(node1[key], dict) and isinstance(
-                node2[key], dict
-        ):
+    for key in all_keys:
+        if key in common_keys:
+            if isinstance(node1[key], dict) and isinstance(
+                    node2[key], dict
+            ):
+                diff.append({
+                    'key': key,
+                    'type': 'nested',
+                    'children': generate(node1[key], node2[key]),
+                })
+
+            elif node1[key] == node2[key]:
+                diff.append({
+                    'key': key,
+                    'type': 'unchanged',
+                    'value': node1[key],
+                })
+            else:
+                diff.append({
+                    'key': key,
+                    'type': 'changed',
+                    'old_value': node1[key],
+                    'new_value': node2[key],
+                })
+
+        elif key in removed_keys:
             diff.append({
                 'key': key,
-                'type': 'nested',
-                'children': generate(node1[key], node2[key]),
-            })
-
-        elif node1[key] == node2[key]:
-            diff.append({
-                'key': key,
-                'type': 'unchanged',
+                'type': 'removed',
                 'value': node1[key],
             })
+
         else:
             diff.append({
                 'key': key,
-                'type': 'changed',
-                'old_value': node1[key],
-                'new_value': node2[key],
+                'type': 'added',
+                'value': node2[key],
             })
-
-    for key in removed_keys:
-        diff.append({
-            'key': key,
-            'type': 'removed',
-            'value': node1[key],
-        })
-
-    for key in added_keys:
-        diff.append({
-            'key': key,
-            'type': 'added',
-            'value': node2[key],
-        })
     return diff
