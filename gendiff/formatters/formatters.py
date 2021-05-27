58777b864bff2c7a3346726b0fbd89e32fd589eb
denis <yudaev1@gmail.com>
2021-05-27 13:32:47 +0300

    Update formatters.py

--- gendiff/formatters/formatters.py
+++ gendiff/formatters/formatters.py
@@ -1,8 +1,9 @@
 from gendiff.formatters import stylish_format, plain_format, json_format
 
 
-map_formatters = {
-    'stylish': stylish_format,
-    'plain': plain_format,
-    'json': json_format,
-}
+def format_diff(diff, format):
+    if format == 'stylish':
+        return stylish_format.format_diff(diff)
+    elif format == 'plain':
+        return plain_format.format_diff(diff)
+    return json_format.format_diff(diff)
