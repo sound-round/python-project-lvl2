3645991bc76b91efb291ae0044b0a5477b4d0bc7
denis <yudaev1@gmail.com>
2021-06-04 12:12:29 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -22,33 +22,35 @@ def get_fixture_path(fixture_name):
     )
 
 
-@pytest.mark.parametrize('first_file, second_file, expected_result', [
+@pytest.mark.parametrize('first_file, second_file', [
     (
-        get_fixture_path(('file1.json')),
-        get_fixture_path(('file2.json')),
-        get_fixture_path(('result.stylish')),
+        get_fixture_path('file1.json'),
+        get_fixture_path('file2.json'),
     ),
     (
-        get_fixture_path(('file1.yaml')),
-        get_fixture_path(('file2.yaml')),
-        get_fixture_path(('result.stylish')),
+        get_fixture_path('file1.yaml'),
+        get_fixture_path('file2.yaml'),
     ),
 ])
-def test_generate_diff_default(first_file, second_file, expected_result):
+def test_generate_diff_default(first_file, second_file):
     assert generate_diff(
         first_file,
         second_file,
-    ) == read(expected_result)
+    ) == read(get_fixture_path('result.stylish'))
 
 
 @pytest.mark.parametrize('first_file, second_file', [
     (
-        get_fixture_path(('file1.json')),
-        get_fixture_path(('file2.json')),
+        get_fixture_path('file1.json'),
+        get_fixture_path('file2.json'),
     ),
     (
-        get_fixture_path(('file1.yaml')),
-        get_fixture_path(('file2.yaml')),
+        get_fixture_path('file1.yaml'),
+        get_fixture_path('file2.yaml'),
+    ),
+    (
+        get_fixture_path('file1.json'),
+        get_fixture_path('file2.yaml'),
     ),
 ])
 def test_generate_diff_formats(first_file, second_file):
