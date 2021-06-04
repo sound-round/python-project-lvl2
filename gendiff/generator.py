3ff1683d901cc2d757abb068ec6f8964b215534b
denis <yudaev1@gmail.com>
2021-06-04 13:57:28 +0300

    Update generator.py

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -5,6 +5,11 @@ from gendiff.formatters import formatters
 from gendiff import tree
 import pathlib
 from gendiff import file_parser
+import os
+
+
+def get_format_file(path):
+    return os.path.splitext(path)[1][1:]
 
 
 def generate_diff(first_file_path,
@@ -15,12 +20,12 @@ def generate_diff(first_file_path,
     in different formats (stylish, plain, json).
 
     """
-    first_file = open(first_file_path)
-    second_file = open(second_file_path)
-    format_first_file = pathlib.PurePath(first_file_path).name.split('.')[1]
-    format_second_file = pathlib.PurePath(second_file_path).name.split('.')[1]
-    parsed_first_file = file_parser.parse_file(first_file, format_first_file)
-    parsed_second_file = file_parser.parse_file(second_file, format_second_file)
+    first_file_data = open(first_file_path)
+    second_file_data = open(second_file_path)
+    first_file_format = get_format_file(first_file_path)
+    second_file_format = get_format_file(second_file_path)
+    parsed_first_file = file_parser.parse_file(first_file_data, first_file_format)
+    parsed_second_file = file_parser.parse_file(second_file_data, second_file_format)
 
     diff = tree.build_diff(parsed_first_file, parsed_second_file)
     return formatters.format_diff(diff, format)
