b7936228f6326a9adf5c654133ebf3b5786ac97a
denis <yudaev1@gmail.com>
2021-06-15 11:16:42 +0300

    Update file_parser.py

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -2,12 +2,9 @@ import json
 import yaml
 
 
-yaml_format = ['yaml', 'yml']
-
-
 def parse(file, format):
     if format == 'json':
         return json.load(file)
-    if format in yaml_format:
+    if format in ['yaml', 'yml']:
         return yaml.load(file, Loader=yaml.FullLoader)
     raise ValueError('Unknown file format.')
