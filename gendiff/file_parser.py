8199a145f4c820d2136fc23872d1ad8f55d0e796
denis <yudaev1@gmail.com>
2021-06-22 11:10:06 +0300

    Refactor file_parser.py

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -5,11 +5,11 @@ import yaml
 INPUT_FORMATS = ['json', 'yaml']
 
 
-def parse(file, format):
+def parse(file_data, format):
     if format == 'json':
-        return json.load(file)
+        return json.load(file_data)
     if format in ['yaml', 'yml']:
-        return yaml.load(file, Loader=yaml.FullLoader)
+        return yaml.load(file_data, Loader=yaml.FullLoader)
     raise ValueError(
         f'Unknown input format: {format}. '
         f'Choose from {", ".join(INPUT_FORMATS)}.'
